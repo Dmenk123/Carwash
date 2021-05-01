35eda2a46489b0b19112b17767ca138969e7ba33
nurchyn <nurcahyono320@gmail.com>
2021-05-01 10:48:40 +0700

    sadd

--- application/modules/penjualan/controllers/Penjualan.php
+++ application/modules/penjualan/controllers/Penjualan.php
@@ -141,8 +141,8 @@ class Penjualan extends CI_Controller {
 						}
 						
 						.ticket {
-							width: 175px;
-							max-width: 175px;
+							width: 172px;
+							max-width: 172px;
 						}
 						
 						img {
